7fa7be03c452447163d9c28adb9108a3ee1969b6
Oke Emmanuel <speakingatoms@gmail.com>
2020-04-07 20:34:07 +0100

    Updates

---
+++ precache-manifest.4721a8a1748a6ef92830856e0355b096.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45defd10dc8d12ba42f38f8c682a7a8d",
+    "url": "/oldfaces/index.html"
+  },
+  {
+    "revision": "77f33b2e7481c08ccedc",
+    "url": "/oldfaces/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "9de5a3cfd663b675b3b0",
+    "url": "/oldfaces/static/css/main.b7db08c1.chunk.css"
+  },
+  {
+    "revision": "77f33b2e7481c08ccedc",
+    "url": "/oldfaces/static/js/2.0396895a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oldfaces/static/js/2.0396895a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de5a3cfd663b675b3b0",
+    "url": "/oldfaces/static/js/main.74e02720.chunk.js"
+  },
+  {
+    "revision": "c2aaecd7d3d0224ffc34",
+    "url": "/oldfaces/static/js/runtime-main.4849f811.js"
+  },
+  {
+    "revision": "85d1a60b036dc6865cce9e66caa4591a",
+    "url": "/oldfaces/static/media/Chococooky.85d1a60b.woff"
+  },
+  {
+    "revision": "af0ec415437277bb43f3d5bb69baf009",
+    "url": "/oldfaces/static/media/RandomXBold-ZVEdB.af0ec415.otf"
+  }
+]);
